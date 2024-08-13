5ccc6a154d7bef5f1017648ae63a86cdff0cbf90
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-08-13 17:29:45 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = f"{first_name} {last_name}"
+    print(f"{full_name.lower()}")
+    print(f"{full_name.title()}")
+    print(f"{full_name.upper()}")
+    print(f"\t{full_name.lower()}")
